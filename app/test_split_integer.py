ec273f11b08d4f669494c26d4b2a0a7b19965f29
Sergey <sobka2702@gmail.com>
2026-04-23 02:27:30 +0300

    added test for split 32/6

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,10 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value = 17
-    number_of_parts = 4
-
-    assert sum(split_integer(value, number_of_parts)) == value
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -16,8 +13,12 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
-def test_should_return_sorted_parts_when_they_are_not_equal() -> None:
+def test_should_split_value_according_to_example_with_remainder() -> None:
     assert split_integer(17, 4) == [4, 4, 4, 5]
+
+
+def test_should_split_32_into_6_parts() -> None:
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
