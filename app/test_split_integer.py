3002c678318723aa8b39f1d6633dbb49e60d3858
Sergey <sobka2702@gmail.com>
2026-04-23 02:20:39 +0300

    fix sum func from ai mentor

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,9 +5,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 17
     number_of_parts = 4
 
-    result = split_integer(value, number_of_parts)
-
-    assert sum(result) == value
+    assert sum(split_integer(value, number_of_parts)) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -18,11 +16,8 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-
-    assert result == [4, 4, 4, 5]
-    assert result == sorted(result)
+def test_should_return_sorted_parts_when_they_are_not_equal() -> None:
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
